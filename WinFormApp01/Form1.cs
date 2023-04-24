8fb6df220476b673f90d7db79d1343ce4b6ec953
ansuke21 <f.horii@harukasystem.co.jp>
2023-04-24 10:15:14 +0900

    プロジェクト ファイルを追加します。

---
+++ WinFormApp01/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApp01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.lgsInit();
+        }
+        private void lgsInit()
+        {
+            this.textBox起点.Text = "13102";
+            this.textBox終点.Text = "27127";
+            this.textBoxWebApiKey.Text = "8779c23a-527e-4cba-8a50-32969cd94470";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClgsWebKiroTeiKyori01.ClgsWebKiroTeiKyori01 ClgsWebKiroTei = new ClgsWebKiroTeiKyori01.ClgsWebKiroTeiKyori01();
+            ClgsWebKiroTei.Kiten = this.textBox起点.Text;
+            ClgsWebKiroTei.Shuuten = this.textBox終点.Text;
+            ClgsWebKiroTei.WebApiKey = this.textBoxWebApiKey.Text;
+            // プロキシ認証がある場合は設定します
+            ClgsWebKiroTei.ProxyServer = "";
+            ClgsWebKiroTei.ProxyUserName = "";
+            ClgsWebKiroTei.ProxyPassword = "";
+
+            Boolean blnCond = ClgsWebKiroTei.lgsKyoriKmKeisan();
+            this.textBox主要距離Km.Text = ClgsWebKiroTei.ShuyouKyoriKm.ToString();
+            this.textBox最短距離Km.Text = ClgsWebKiroTei.SaitanKyoriKm.ToString();
+            this.textBoxError.Text = ClgsWebKiroTei.Error;
+        }
+
+
+    }
+}
